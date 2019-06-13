abcc6dddffb2c6bc0a5ed41e947d9a2ec2571782
Victor Ng <victor@crankycoder.com>
2019-06-13 10:32:24 -0400

    redirect stdout+stderr to /dev/null

--- setup_pyenv.sh
+++ setup_pyenv.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pyenv virtualenv 3.5.7 python_mozetl
-pyenv activate python_mozetl
+pyenv activate python_mozetl > /dev/null 2>&1
 wget --quiet https://raw.githubusercontent.com/crankycoder/srg-python_mozetl/master/etl-requirements.txt -O /tmp/requirements.txt
 pip install --upgrade pip
 pip install -r /tmp/requirements.txt
